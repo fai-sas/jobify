15fab29a73440db877fae57bc81f1b6fa247e031
fai-sas <craftedbyfaisal@gmail.com>
2022-10-22 23:37:29 +0600

    first commit

---
+++ client/src/components/index.js
@@ -0,0 +1,39 @@
+import Logo from './Logo'
+import Alert from './Alert'
+import FormRow from './FormRow'
+import Navbar from './Navbar'
+import SmallSidebar from './SmallSidebar'
+import BigSidebar from './BigSidebar'
+import FormRowSelect from './FormRowSelect'
+import SearchContainer from './SearchContainer'
+import JobsContainer from './JobsContainer'
+import Job from './Job'
+import JobInfo from './JobInfo'
+import Loading from './Loading'
+import StatsContainer from './StatsContainer'
+import ChartsContainer from './ChartsContainer'
+import StatsItem from './StatsItem'
+import BarChart from './BarChart'
+import AreaChart from './AreaChart'
+import PageBtnContainer from './PageBtnContainer'
+
+export {
+  Logo,
+  Alert,
+  FormRow,
+  BigSidebar,
+  SmallSidebar,
+  Navbar,
+  FormRowSelect,
+  SearchContainer,
+  JobsContainer,
+  Job,
+  JobInfo,
+  Loading,
+  StatsContainer,
+  ChartsContainer,
+  StatsItem,
+  BarChart,
+  AreaChart,
+  PageBtnContainer,
+}
